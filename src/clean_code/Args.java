530150c2d611dd600924234b25bc5c421140820e
lichuangbo <15503609389@163.com>
2022-01-03 16:04:30 +0800

    在布尔基础上拓展了string类型命令

--- src/clean_code/Args.java
+++ src/clean_code/Args.java
@@ -1,9 +1,7 @@
 package clean_code;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.text.ParseException;
+import java.util.*;
 
 public class Args {
   private String schema;
@@ -11,15 +9,19 @@ public class Args {
   private boolean valid;
   private Set<Character> unexpectedArguments = new TreeSet<>();
   private Map<Character, Boolean> booleanArgs = new HashMap<>();
-  private int numberOfArguments = 0;
+  private Map<Character, String> stringArgs = new HashMap<>();
+  private List<Character> argsFound = new ArrayList<>();
+  private int currentArgument;
+  private char errorArgument = '\0';
+  private ErrorCode errorCode = ErrorCode.OK;
 
-  public Args(String schema, String[] args) {
+  public Args(String schema, String[] args) throws ParseException {
     this.schema = schema;
     this.args = args;
     valid = parse();
   }
 
-  private boolean parse() {
+  private boolean parse() throws ParseException {
     if (schema.length() == 0 && args.length == 0) {
       return true;
     }
@@ -28,31 +30,66 @@ public class Args {
     return unexpectedArguments.size() == 0;
   }
 
-  private boolean parseSchema() {
+  private boolean parseSchema() throws ParseException {
     for (String element : schema.split(",")) {
-      parseSchemaElement(element);
+      if (element.length() > 0) {
+        String trimmedElement = element.trim();
+        parseSchemaElement(trimmedElement);
+      }
     }
     return true;
   }
 
-  private void parseSchemaElement(String element) {
-    if (element.length() == 1) {
-      parseBooleanSchemaElement(element);
+  private void parseSchemaElement(String element) throws ParseException {
+    char elementId = element.charAt(0);
+    validateSchemaElementId(elementId);
+    // 按照规则截取字符串，判断单个schema类型，l:布尔,p#:整数,d*:字符串
+    String elementTail = element.substring(1);
+    if (isBooleanSchemaElement(elementTail)) {
+      parseBooleanSchemaElement(elementId);
+    } else if (isStringSchemaElement(elementTail)) {
+      parseStringSchemaElement(elementId);
     }
   }
 
-  private void parseBooleanSchemaElement(String element) {
-    char c = element.charAt(0);
-    if (Character.isLetter(c)) {
-      booleanArgs.put(c, false);
+  // 解析错误向上传递
+  private void validateSchemaElementId(char elementId) throws ParseException {
+    if (!Character.isLetter(elementId)) {
+      throw new ParseException("Bad character: " + elementId + "in Args format: " + schema, 0);
     }
+  }
+
+  private boolean isBooleanSchemaElement(String elementTail) {
+    return elementTail.length() == 0;
+  }
+
+  private void parseBooleanSchemaElement(char elementId) {
+    booleanArgs.put(elementId, false);
+  }
+
+  private boolean isStringSchemaElement(String elementTail) {
+    return elementTail.equals("*");
+  }
+
+  private void parseStringSchemaElement(char elementId) {
+    stringArgs.put(elementId, "");
   }
 
   private boolean parseArguments() {
-    for (String arg : args) {
+    for (currentArgument = 0; currentArgument < args.length; currentArgument++) {
+      String arg = args[currentArgument];
       parseArgument(arg);
     }
     return true;
+  }
+
+  private void parseElement(char argChar) {
+    if (setArgument(argChar)) {
+      argsFound.add(argChar);
+    } else {
+      unexpectedArguments.add(argChar);
+      valid = false;
+    }
   }
 
   private void parseArgument(String arg) {
@@ -67,25 +104,61 @@ public class Args {
     }
   }
 
-  private void parseElement(char argChar) {
+  private boolean setArgument(char argChar) {
+    boolean setSuccess = true;
     if (isBoolean(argChar)) {
-      numberOfArguments++;
       setBooleanArg(argChar, true);
+    } else if (isString(argChar)) {
+      setStringArg(argChar, "");
     } else {
-      unexpectedArguments.add(argChar);
+      setSuccess = false;
     }
-  }
 
-  private boolean isBoolean(char argChar) {
-    return booleanArgs.containsKey(argChar);
+    return setSuccess;
   }
 
   private void setBooleanArg(char argChar, boolean value) {
     booleanArgs.put(argChar, value);
   }
 
+  private boolean isBoolean(char argChar) {
+    return booleanArgs.containsKey(argChar);
+  }
+
+  private boolean isString(char argChar) {
+    return stringArgs.containsKey(argChar);
+  }
+
+  private void setStringArg(char argChar, String value) {
+    // -d与-l不同，-d后可以追加字符串，不追加默认字符串为空
+    // 定义全部变量是为了跳过[追加的字符串]，进入下一个命令
+    currentArgument++;
+    try {
+      stringArgs.put(argChar, args[currentArgument]);
+      // 当出现数组越界异常时，说明命令后没有追加字符串，记录起来
+    } catch (ArrayIndexOutOfBoundsException e) {
+      valid = false;
+      errorArgument = argChar;
+      errorCode = ErrorCode.MISSING_STRING;
+    }
+  }
+
   public int cardinality() {
-    return numberOfArguments;
+    return argsFound.size();
+  }
+
+  public String errorMessage() throws Exception {
+    if (unexpectedArguments.size() > 0) {
+      return unexpectedArgumentMessage();
+    } else {
+      switch (errorCode) {
+        case MISSING_STRING:
+          return String.format("Could not find string parameter for -%c.", errorArgument);
+        case OK:
+          throw new Exception("TILT: should not get here.");
+      }
+      return "";
+    }
   }
 
   public String usage() {
@@ -96,12 +169,8 @@ public class Args {
     }
   }
 
-  public String errorMessage() {
-    if (unexpectedArguments.size() > 0) {
-      return unexpectedArgumentMessage();
-    } else {
-      return "";
-    }
+  public boolean getBoolean(char arg) {
+    return falseIfNull(booleanArgs.get(arg));
   }
 
   private String unexpectedArgumentMessage() {
@@ -114,8 +183,24 @@ public class Args {
     return message.toString();
   }
 
-  public boolean getBoolean(char arg) {
-    return booleanArgs.get(arg);
+  private boolean falseIfNull(Boolean b) {
+    return b == null ? false : b;
+  }
+
+  public String getString(char arg) {
+    return blankIfNull(stringArgs.get(arg));
+  }
+
+  private String blankIfNull(String s) {
+    return s == null ? "" : s;
+  }
+
+  public boolean has(char arg) {
+    return argsFound.contains(arg);
+  }
+
+  enum ErrorCode {
+    OK, MISSING_STRING
   }
 
   public boolean isValid() {
--- src/clean_code/ArgsTest.java
+++ src/clean_code/ArgsTest.java
@@ -1,17 +1,33 @@
 package clean_code;
 
+import java.text.ParseException;
+
+/**
+ * Argument需求的理解
+ * https://gitee.com/chengkunxf/argument
+ */
 public class ArgsTest {
   public static void main(String[] args) {
-    args = new String[]{"-l", "-m", "-l"};
-    Args myArgs = new Args("l", args);
+    Args myArgs = null;
+    try {
+//      myArgs = new Args("l,d*", new String[]{"-l", "-d", "/usr/local"});
+      myArgs = new Args("l,d*", new String[]{"-l", "-d"});
+//      myArgs = new Args("l,d*", new String[]{"-d", "/usr/local"});
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
     boolean p1 = myArgs.getBoolean('l');
     System.out.println("result:" + p1);
-//    boolean p2 = myArgs.getBoolean('m');
-//    System.out.println("result:" + p2);
+    String p2 = myArgs.getString('d');
+    System.out.println("result:" + p2);
 
     System.out.println("valid:" + myArgs.isValid());
     System.out.println("valid arguments:" + myArgs.cardinality());
     System.out.println("schema:" + myArgs.usage());
-    System.out.println("error message:" + myArgs.errorMessage());
+    try {
+      System.out.println("error message:" + myArgs.errorMessage());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
